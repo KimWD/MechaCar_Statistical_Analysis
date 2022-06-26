f3069092d95fe838e5b244162153df95efd672a0
Kimberly Wagner-Dabbour <kimberlydabbour@gmail.com>
2022-06-25 21:39:57 -0400

    challenge

---
+++ MechaCar_Statistical_Analysis.R
@@ -0,0 +1,14 @@
+#Deliverable #1
+
+#load dplyr 
+library(dplyr)
+
+#import & read file
+MechaCar <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+
+#perform linear regression using lm()
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data= MechaCar)
+
+#perform summary
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data= MechaCar))
+
